d45ce92c301b2a58089251ea1918f716c2244bac
Victoria <vitoria.noronha@gmail.com>
2026-01-28 20:32:10 -0300

    Exemplo do uso de stream

--- Bootcamp/src/CollectionFrameworkAPI/Map/Main.java
+++ Bootcamp/src/CollectionFrameworkAPI/Map/Main.java
@@ -1,4 +1,21 @@
 package Map;
 
 public class Main {
+    public static void main (String [] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Camila" ,786786);
+        agendaContatos.adicionarContato("Maria" ,474657);
+        agendaContatos.adicionarContato("Vanessa" ,674674);
+        agendaContatos.adicionarContato("Elisa" ,567567);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Maria");
+        agendaContatos.exibirContatos();
+
+
+
+
+    }
 }
